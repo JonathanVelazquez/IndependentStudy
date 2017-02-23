b848770bdf8534ecd950245b51b6f6b051dcc069
“JonathanVelazquez” <jvelaz07@uw.edu>
2017-02-23 08:59:32 -0800

    Google Authentication Added
    Google Authentication Added

---
+++ app/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://jvelaz07:quincy432@olympia.modulusmongo.net:27017/i4qijeQo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
